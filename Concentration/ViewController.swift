64cfba2dc3d948ff56e522bffde8f7937bf22c84
Michael <michael@adl-crm.uk>
2017-12-20 10:27:50 +0000

    Push

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Michael Owen on 19/12/2017.
+//  Copyright © 2017 Michael Owen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardbuttons: [UIButton]!
+    
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardbuttons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Chosen card not in cardbuttons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5695850849, blue: 0, alpha: 1)
+        } else {
+          button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+
+}
+
